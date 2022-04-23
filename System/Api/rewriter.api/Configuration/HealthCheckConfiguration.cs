f932373b617bf279b96a2d56556a6f5d989eea17
wolloh <67105296+wolloh@users.noreply.github.com>
2022-04-23 21:58:08 +0300

    Add project files.

---
+++ System/Api/rewriter.api/Configuration/HealthCheckConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace rewriter.api.Configuration
+{
+    public static class HealthCheckConfiguration
+    {
+        public static IServiceCollection AddAppHealthCheck(this IServiceCollection services)
+        {
+            services.AddHealthChecks();
+            return services;
+        }
+        
+        public static WebApplication UseAppHealthCheck(this WebApplication app)
+        {
+            app.MapHealthChecks("/health");
+            return app;
+        }
+    }
+
+}
